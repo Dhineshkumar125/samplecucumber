37acf47f2e756765e75c14058410725f1737d75d
subhashini <s.a.jayavelmurugan@accenture.com>
2019-09-27 09:31:14 +0530

    updating step definitions

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,5 +1,23 @@
 package skeleton;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class Stepdefs {
- 
+
+@Given("user is on the login page")
+public void user_is_on_the_login_page() {
+  System.out.println("driveris launched and navigated to login page");
+}
+
+@When("user provides the correct credentials")
+public void user_provides_the_correct_credentials() {
+   
+}
+
+@Then("user rendered to TestMeApp home page")
+public void user_rendered_to_TestMeApp_home_page() {
+    
+}
 }
