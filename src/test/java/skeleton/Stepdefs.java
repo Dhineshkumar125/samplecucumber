45ee82d57d5dde8309d0185b25af7715bd828d8d
subhashini <s.a.jayavelmurugan@accenture.com>
2019-09-27 10:56:36 +0530

    driver update

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -8,12 +8,12 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Stepdefs {
-	WebDriver driver;
+	
 @Given("user is on the login page")
 public void user_is_on_the_login_page() {
   System.out.println("driveris launched and navigated to login page");
   System.setProperty("webdriver.chrome.driver", "C:\\Users\\A08019DIRP_C2C.02.02\\Downloads\\chromedriver.exe");
-   driver= new ChromeDriver();
+  WebDriver driver= new ChromeDriver();
   driver.navigate().to("http://10.232.237.143:443/TestMeApp/fetchcat.htm");
 }
 
@@ -24,7 +24,7 @@ public void user_provides_the_correct_credentials() {
 
 @Then("user rendered to TestMeApp home page")
 public void user_rendered_to_TestMeApp_home_page() {
-    driver.close();
+    
     System.out.println("driver is closed");
     
     
