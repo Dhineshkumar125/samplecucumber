d9589dae4164d6ba09d0b0318b6f9a73b6a4c2e3
subhashini <s.a.jayavelmurugan@accenture.com>
2019-09-27 09:39:45 +0530

    driver details updated

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,14 +1,20 @@
 package skeleton;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Stepdefs {
-
+	WebDriver driver;
 @Given("user is on the login page")
 public void user_is_on_the_login_page() {
   System.out.println("driveris launched and navigated to login page");
+  System.setProperty("windows.chrome.driver", "C:\\Users\\A08019DIRP_C2C.02.02\\Downloads\\chromedriver.exe");
+   driver= new ChromeDriver();
+  driver.navigate().to("google.com");
 }
 
 @When("user provides the correct credentials")
@@ -18,6 +24,9 @@ public void user_provides_the_correct_credentials() {
 
 @Then("user rendered to TestMeApp home page")
 public void user_rendered_to_TestMeApp_home_page() {
+    driver.close();
+    System.out.println("driver is closed");
+    
     
 }
 }
